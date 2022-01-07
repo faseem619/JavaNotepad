b9770590c3df8c71684be3fc4209f4c9db46ad4d
faseem619 <faseem619@gmail.com>
2022-01-07 15:15:00 +0530

    added font picker and menubar skeleton

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(String[] args) {
+        if(System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0) //checking if os is mac os
+		{
+		 System.setProperty("apple.laf.useScreenMenuBar", "true");
+		  System.setProperty(
+            "com.apple.mrj.application.apple.menu.about.name", "Name");
+		}
         new Notepad();
     }
 }
--- Notepad.java
+++ Notepad.java
@@ -1,5 +1,9 @@
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
@@ -10,18 +14,26 @@ import javax.swing.event.ChangeListener;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Notepad {
-
+public class Notepad implements ActionListener {
+    JComboBox<String> fontpicker; 
+    JTextArea textArea;
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem exiItem;
+    String [] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames(); 
     Notepad(){
         JFrame frame = new JFrame("Notepad");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
 
-        JTextArea textArea = new JTextArea();
+         textArea = new JTextArea();
         textArea.setBackground(new Color(0xf9fafa));
         textArea.setFont(new Font("Arial",Font.PLAIN,18));
         textArea.setWrapStyleWord(true);
@@ -33,11 +45,12 @@ public class Notepad {
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         
         JPanel topPanel = new JPanel();
-        JLabel sizLabel = new JLabel("Font Size:");
+        JLabel sizLabel = new JLabel("Size:");
+        JLabel fontLabel = new JLabel("Font :");
         JSpinner sizeSpinner = new JSpinner();
         sizeSpinner.setPreferredSize(new Dimension(45,25));
         sizeSpinner.setValue(18);
-        sizeSpinner.addChangeListener(new ChangeListener() {
+        sizeSpinner .addChangeListener(new ChangeListener() {
 
             @Override
             public void stateChanged(ChangeEvent e) {
@@ -46,17 +59,44 @@ public class Notepad {
             }
             
         });
+
+        fontpicker = new JComboBox<String>(fonts);
+        fontpicker.setPreferredSize(new Dimension(150,25));
+        fontpicker.setSelectedItem("Arial");
+        fontpicker.addActionListener(this);
+        
+        //-------menubar-----
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        exiItem = new JMenuItem("Exit");
+        fileMenu.add(exiItem);
+        menuBar.add(fileMenu);
+        exiItem.addActionListener(this);
+
+        /* menubar*/
+
+        topPanel.add(fontLabel);
+        topPanel.add(fontpicker);
         topPanel.add(sizLabel);
         topPanel.add(sizeSpinner);
 
-        
-
+        frame.setJMenuBar(menuBar);
         frame.add(scrollPane,BorderLayout.CENTER);
         frame.add(topPanel,BorderLayout.NORTH);
         frame.pack();
         frame.setVisible(true);
         textArea.requestFocusInWindow();
 
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       if(e.getSource()==fontpicker){  
+            textArea.setFont(new Font(fontpicker.getSelectedItem().toString(),textArea.getFont().getStyle(),textArea.getFont().getSize()));
+       }
+       else if(e.getSource()==exiItem){
+           System.exit(0);
+       }
     }
     
 }
